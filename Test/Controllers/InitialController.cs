8ba10fc4304fca831fcab72febc536a6b8e2d4f1
Cachap <maksim.dorofeev.95@mail.ru>
2025-12-26 16:56:49 +0300

    feat: initial

---
+++ Test/Controllers/InitialController.cs
@@ -0,0 +1,35 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace Test.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InitialController(IConfiguration configuration) : ControllerBase
+    {
+        [HttpGet("health")]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+		[HttpGet("db-ping")]
+		public async Task<ActionResult> DbPing()
+		{
+			var connectionString = configuration.GetConnectionString("Default");
+
+			await using var connection = new NpgsqlConnection(connectionString);
+			await connection.OpenAsync();
+
+			await using var command = new NpgsqlCommand("SELECT 1", connection);
+			var result = await command.ExecuteScalarAsync();
+
+			return Ok(new
+			{
+				status = "ok",
+				db = result
+			});
+		}
+	}
+}
